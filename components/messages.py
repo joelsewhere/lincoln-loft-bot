6e995f195bfbb41d521f5d471f3e63001fa070bb
joelsewhere <joelsewhere@gmail.com>
2020-12-27 10:46:21 -0600

    environment variables

--- components/messages.py
+++ components/messages.py
@@ -1,28 +1,31 @@
 import discord
+import os
 
 
 
 class Messages:
 
     async def welcome_message(self, member):
-
+        CALENDAR = os.environ.get('CALENDAR')
+        DRIVE = os.environ.get('DRIVE')
+        HOUSE = os.environ.get('HOUSE')
         await member.create_dm()                                        
         resources = discord.Embed(title="Here are some roommate resources!", color=0xa25d54)
         resources.set_thumbnail(url="https://chicagoagentmagazine.com/wp-content/uploads/2012/04/PIN.jpg")
-        resources.add_field(name="========================\nThe Loft Calendar", value="Click **[here](https://calendar.google.com/calendar/u/3?cid=bG9mdDMwMzZAZ21haWwuY29t)**\
+        resources.add_field(name="========================\nThe Loft Calendar", value=f"Click **[here]({CALENDAR})**\
                                                             to view the loft calendar\n> We use google calendar to schedule time in the theatre!\
                                                             The calendar can be viewed by everyone. If you'd like to *schedule* events on the calendar,\
                                                             reach out to one of your roommates to be given edit access!", inline=False)
         resources.add_field(name="========================\nRoommate Google Drive Folder", 
-                            value="""Click\
-                                    **[here](https://drive.google.com/drive/folders/1T4VFb__2OJMa7D3ORVkdVKJ16D_ChvDs?usp=sharing)**\
+                            value=f"""Click\
+                                    **[here]({DRIVE})**\
                                         to open the roommate folder.\n> If you can't find a light switch, or have questions about\
                                         how things work, this google drive folder is a good place to check.\
                                 """, inline=False)
 
         resources.add_field(
             name='========================\nThe House Channel',
-            value = f"""> Be sure to check out the **[welcome message](https://discord.com/channels/745756477855367268/745756477855367271/745806031447261214)** \
+            value = f"""> Be sure to check out the **[welcome message]({HOUSE})** \
                 in the The House Channel\
                 and if it feels right, drop a message introducing yourself! _We're happy you're here!_ <:emoji_name:792429574565789717>\n\n***Welcome to The Lincoln Loft!***"""
         )
